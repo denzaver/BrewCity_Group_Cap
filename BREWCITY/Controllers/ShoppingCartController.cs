84c5e6ef2072cfc68940c2371c6398b336b90596
Denis Zavertyayev <denisz2277@gmail.com>
2021-04-14 11:12:04 -0700

    Created services within the startup class to allow the creation of shopping carts for users, as well as an addHttpContectAccessor and addSession service, linking them in the configure method by adding an app.usesession method call.

---
+++ BREWCITY/Controllers/ShoppingCartController.cs
@@ -0,0 +1,19 @@
+﻿using BREWCITY.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ShoppingCart shoppingCart;
+
+        public ShoppingCartController()
+        {
+
+        }
+    }
+}
