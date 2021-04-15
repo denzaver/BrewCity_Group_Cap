23753e8124474b032df079ad3b004c373cdf86c1
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-14 22:47:09 -0500

    Location.cs added to models. Can apply to either bewery or customers, as it uses Identity user.

---
+++ BREWCITY/Models/Location.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class Location
+    {
+        public string State { get; set; }
+        public string City { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+    }
+}
