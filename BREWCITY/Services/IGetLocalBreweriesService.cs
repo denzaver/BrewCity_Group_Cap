5bba691c852e3b7f647c43967e79e8592fa4b31f
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-14 21:32:00 -0500

    Services folder added for api.

---
+++ BREWCITY/Services/IGetLocalBreweriesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BREWCITY.Models;
+
+namespace BREWCITY.Services
+{
+    public interface IGetLocalBreweriesService
+    {
+        Task<List<JsonBrewery>> GetLocalBreweries(string state);
+    }
+}
