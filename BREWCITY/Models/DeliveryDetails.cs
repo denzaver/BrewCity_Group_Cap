0267870a32bd17bcae378d6d77807d6dd9f644af
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-15 00:42:42 -0500

    Delivery Model class added to faciltiate checkout process for customer.

---
+++ BREWCITY/Models/DeliveryDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class DeliveryDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        //or transaction id
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        [Display(Name = "Enter the name of the responsible party receiving the shipment.")]
+
+        public string ResponsiblePartyName { get; set; }
+        [Display(Name = "Enter the best phone number for the delivery contact:")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Delivery street number and address:")]
+        public string Street { get; set; }
+        [Display(Name = "Delivery City:")]
+        public string City { get; set; }
+        [Display(Name = "Delivery State")]
+        public string State { get; set; }
+        [Display(Name = "Enter any required state alcohol licence numbers for your state. Delivery may be delayed if information is not provided.")]
+        public string StateAlcoholLicenceNumber { get; set; }
+    }
+}
