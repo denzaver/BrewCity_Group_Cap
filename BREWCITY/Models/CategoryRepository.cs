1979e8f976055513c430ce72e061956588d3819f
Denis Zavertyayev <denisz2277@gmail.com>
2021-04-14 21:53:46 -0700

    Created a CategoryRepository model that inherits from the ICategoryRepository and will implement the interface.

---
+++ BREWCITY/Models/CategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class CategoryRepository : ICategoryRepository // this class implements the ICategory interface
+    {
+        public IEnumerable<Category> GetAllCategories => throw new NotImplementedException();
+    }
+}
