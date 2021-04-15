bb35ee47bbc026f3dc4c35677eedb9747b099bf9
Denis Zavertyayev <denisz2277@gmail.com>
2021-04-14 21:33:45 -0700

    Created a Catagory model as well as an IBeerRepository model to process the data of the Beer model through dependancy injection using interfaces.

---
+++ BREWCITY/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class Category
+    {
+        public int CatagoryId { get; set; }
+        public string CatagoryName { get; set; }
+        public string CatagoryDescription { get; set; }
+        public List<Beer> Beers { get; set; }
+    }
+}
---
+++ BREWCITY/Models/IBeerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public interface IBeerRepository
+    {
+        IEnumerable<Beer> GetAllBear { get; }
+        IEnumerable<Beer> GetBeerOnSale { get; }
+        Beer GetBeerById(int beerId);
+    }
+}
