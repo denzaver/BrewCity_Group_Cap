fd65c7e368541f439840e9726286593cc86335b5
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-14 22:55:45 -0500

    Location model updated to save location of customer.

--- BREWCITY/Models/Location.cs
+++ BREWCITY/Models/Location.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,12 @@ namespace BREWCITY.Models
 {
     public class Location
     {
-        // class created for the benefit of franchise owners who may have multiple locations
-        public string City { get; set; }
         public string State { get; set; }
+        public string City { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
     }
 }
