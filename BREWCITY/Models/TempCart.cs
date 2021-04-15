57844c1eb3dd8b7fc4bcdce245e89b4e92300420
Brian Hayes <briancampbellhayes@icloud.com>
2021-04-15 18:27:38 -0500

    Adds the temp cart model.

---
+++ BREWCITY/Models/TempCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class TempCart
+    {
+        [Key]
+        public string TempCartId { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey("Beer")]
+        public int BeerId { get; set; }
+        public Beer Beer { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
