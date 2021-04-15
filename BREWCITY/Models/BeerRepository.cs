27e6f26d407bc4e0c209328f291e7d44162a38ef
Denis Zavertyayev <denisz2277@gmail.com>
2021-04-14 22:50:44 -0700

    Completed the login with the Category Repository and passed in a global variable of aplicationDbContext and a method that creates a list of categories. Created a BeerRepository model and possed in the linking global variable ApplicationDbContext, will be creating the logic to pass entered data from teh database.

---
+++ BREWCITY/Models/BeerRepository.cs
@@ -0,0 +1,21 @@
+﻿using BREWCITY.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class BeerRepository : IBeerRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public IEnumerable<Beer> GetAllBear => throw new NotImplementedException();
+
+        public IEnumerable<Beer> GetBeerOnSale => throw new NotImplementedException();
+
+        public Beer GetBeerById(int beerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- BREWCITY/Models/CategoryRepository.cs
+++ BREWCITY/Models/CategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BREWCITY.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,13 @@ namespace BREWCITY.Models
 {
     public class CategoryRepository : ICategoryRepository // this class implements the ICategory interface
     {
-        public IEnumerable<Category> GetAllCategories => throw new NotImplementedException();
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Category> GetAllCategories => _context.Categories;
     }
 }
