39bb2df10fc09db96e6d4b48e2536a7b1bf4bdf7
elezibeth <elezibethmorgan@my.unt.edu>
2021-04-14 22:04:24 -0500

    Model added for city and state for the benefit of franchise owners.

---
+++ BREWCITY/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BREWCITY.Models
+{
+    public class Location
+    {
+        // class created for the benefit of franchise owners who may have multiple locations
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
