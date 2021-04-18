694025b749c92491121f5704d94ea2415b8acbd0
katejoyreed <katejoyreed@gmail.com>
2021-04-18 15:58:45 -0500

    Added migration to deal with pending changes to TempCart model

--- BREWCITY/Migrations/20210418205507_attempting to fix pending database changes.Designer.cs
+++ BREWCITY/Migrations/20210418205507_attempting to fix pending database changes.Designer.cs
@@ -4,14 +4,16 @@ using BREWCITY.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BREWCITY.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210418205507_attempting to fix pending database changes")]
+    partial class attemptingtofixpendingdatabasechanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BREWCITY/Migrations/20210418205507_attempting to fix pending database changes.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BREWCITY.Migrations
+{
+    public partial class attemptingtofixpendingdatabasechanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TempCarts_Beers_BeerId",
+                table: "TempCarts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TempCarts_BeerId",
+                table: "TempCarts");
+
+            migrationBuilder.DropColumn(
+                name: "BeerId",
+                table: "TempCarts");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TempCartId",
+                table: "Beers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Beers_TempCartId",
+                table: "Beers",
+                column: "TempCartId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Beers_TempCarts_TempCartId",
+                table: "Beers",
+                column: "TempCartId",
+                principalTable: "TempCarts",
+                principalColumn: "TempCartId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Beers_TempCarts_TempCartId",
+                table: "Beers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Beers_TempCartId",
+                table: "Beers");
+
+            migrationBuilder.DropColumn(
+                name: "TempCartId",
+                table: "Beers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BeerId",
+                table: "TempCarts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TempCarts_BeerId",
+                table: "TempCarts",
+                column: "BeerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TempCarts_Beers_BeerId",
+                table: "TempCarts",
+                column: "BeerId",
+                principalTable: "Beers",
+                principalColumn: "BeerId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
